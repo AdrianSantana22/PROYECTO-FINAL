dea6dd6b76493a91244013e8faf828c2bc63b6bc
AdrianSantana22 <167695338+AdrianSantana22@users.noreply.github.com>
2024-07-13 23:27:18 -0500

    Commit Proyecto Angular

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+  apiKey: "AIzaSyD-gR8ypStJwkCh3bQKdbuA8cQNFDgYJ1c",
+  authDomain: "becas-y-ayudas-economicas.firebaseapp.com",
+  projectId: "becas-y-ayudas-economicas",
+  storageBucket: "becas-y-ayudas-economicas.appspot.com",
+  messagingSenderId: "850454717280",
+  appId: "1:850454717280:web:90da8435c66be23399b405",
+  measurementId: "G-MX634PVSKX"
+  },
+};
